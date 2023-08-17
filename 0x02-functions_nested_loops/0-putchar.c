7ffc122d94157f646189b8976111df1041f90536
AL-Morchid-Chaymae <almorchidchaymae1@gmail.com>
2023-08-17 15:25:53 -0700

    task1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include "main.h"
 	/**
 	 ** main - print _putchar
-	 *@_putchar : main - print _putchar
 	 * is written by Chaymae
 	 * Return: 0
 	 */
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,15 @@
-#include "main.h"
+#include "holberton.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char c;
 
-
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
-	void print_alphabet_x10(void)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		char ch;
-		int i;
-
-
-		i = 0;
-
-
-		while (i < 10)
-		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchar('\n');
-			i++;
-		}
+		_putchar(c);
 	}
-
+	_putchar('\n');
+}
