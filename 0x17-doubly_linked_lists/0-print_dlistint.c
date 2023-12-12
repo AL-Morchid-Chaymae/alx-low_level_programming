83d4d2c6f184593ccc3540db50dccea2bc514aea
AL-Morchid-Chaymae <almorchidchaymae1@gmail.com>
2023-12-12 04:21:34 -0800

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,44 +1,30 @@
-#include <stdio.h>
-#include <stddef.h> // for size_t
+#include "lists.h"
 
-// Assuming a doubly linked list structure
-struct dlistint_t {
-    int data;
-    struct dlistint_t *prev;
-    struct dlistint_t *next;
-};
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
 
-// Function to print all elements of a dlistint_t list
-size_t print_dlistint(const struct dlistint_t *h) {
-    size_t node_count = 0; // Initialize node count
+	count = 0;
 
-    // Traverse the list and print each element
-    while (h != NULL) {
-        printf("%d\n", h->data);
-        h = h->next; // Move to the next node
-        node_count++; // Increment node count
-    }
+	if (h == NULL)
+		return (count);
 
-    return node_count; // Return the total number of nodes
-}
+	while (h->prev != NULL)
+		h = h->prev;
 
-// Example usage
-int main() {
-    // Create a sample doubly linked list
-    struct dlistint_t node1 = {1, NULL, NULL};
-    struct dlistint_t node2 = {2, NULL, NULL};
-    struct dlistint_t node3 = {3, NULL, NULL};
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
 
-    node1.next = &node2;
-    node2.prev = &node1;
-    node2.next = &node3;
-    node3.prev = &node2;
-
-    // Call the print_dlistint function
-    size_t count = print_dlistint(&node1);
-
-    // Display the number of nodes
-    printf("Number of nodes: %zu\n", count);
-
-    return 0;
+	return (count);
 }
